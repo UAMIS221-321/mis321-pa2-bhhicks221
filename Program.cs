ce7626093113514de1cbb120df0f8848e1204ed0
bhhicks221 <90063321+bhhicks221@users.noreply.github.com>
2022-02-24 17:29:16 -0600

    initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mis321_pa2_bhhicks221
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
